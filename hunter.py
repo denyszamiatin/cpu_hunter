e901fcd0b2d11854f4822e54bdcc62023505175a
Nativeborn <nativeborncitizen@gmail.com>
2016-01-13 18:53:40 +0200

    code review #1

--- hunter.py
+++ hunter.py
@@ -1,15 +1,11 @@
-import requests
 # -*- coding: utf-8 -*-
+import requests
 
-url = 'http://olx.ua/elektronika/kompyutery/q-процессор/'
+URL = 'http://olx.ua/elektronika/kompyutery/q-процессор/'
+
 
 def get_web_page(url):
-    page = requests.get(url)
-    return page._content
+    return requests.get(url).content
 
-def write_to_text_file(filename, data):
-    with open(filename, 'w') as f:
-        f.write(data)
-    f.closed
 
-write_to_text_file('olx.txt', get_web_page(url))
+print get_web_page(URL)
